a114428893ac5d3c6eb274bb6115c70a861d98e6
Mostafa Motahari <mostafamotahari2004@gmail.com>
2021-09-18 00:01:16 +0430

    Add main variables to setting file to access from all around the codes

---
+++ base/settings.py
@@ -0,0 +1,19 @@
+from base.plugins.utilities import get_all_names, get_admins_list, get_all_search_names
+
+OWNER = 1398458529
+
+OWNERS = []
+OWNERS = get_admins_list()
+
+ALL_PLAYERS_NAME = []
+ALL_PLAYERS_NAME = get_all_names()
+
+ALL_SEARCH_PLAYER_NAME = []
+ALL_SEARCH_PLAYER_NAME = get_all_search_names()
+
+USED_PLAYER_NAMES = []
+USED_SEARCH_PLAYER_NAMES = []
+
+data = {
+    "power": "on"
+}
